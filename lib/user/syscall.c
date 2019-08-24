47d1559a9b2ce1f730d5bcd37c5cd82fb2a9cc0b
Xchuanshuo <aixs241460@gmail.com>
2019-08-24 14:24:45 +0800

    添加malloc()和free()系统调用

--- lib/user/syscall.c
+++ lib/user/syscall.c
@@ -57,3 +57,13 @@ uint32_t getpid() {
 uint32_t write(char* str) {
     return _syscall1(SYS_WRITE, str);
 }
+
+/** 申请size字节大小的内存,并返回结果 */
+void* malloc(uint32_t size) {
+   return (void*)_syscall1(SYS_MALLOC, size);
+}
+
+/** 释放ptr指向的内存 */
+void free(void* ptr) {
+   _syscall1(SYS_FREE, ptr);
+}
