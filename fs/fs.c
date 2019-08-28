e8bae67197ad90d1f219a1d8b26934d3b6f6f1f0
Xchuanshuo <aixs241460@gmail.com>
2019-08-28 17:30:33 +0800

    完成文件夹的创建

--- fs/fs.c
+++ fs/fs.c
@@ -470,6 +470,117 @@ int32_t sys_unlink(const char* pathname) {
     return 0;
 }
 
+/** 创建目录pathname,成功返回0,失败返回-1 */
+int32_t sys_mkdir(const char* pathname) {
+    uint8_t rollback_step = 0; // 用与操作失败时回滚各个资源状态
+    void* io_buf = sys_malloc(SECTOR_SIZE * 2);
+    if (io_buf == NULL) {
+        printk("sys_mkdir: sys_malloc for io_buf failed!\n");
+        return -1;
+    }
+    struct path_search_record searched_record;
+    memset(&searched_record, 0, sizeof(struct path_search_record));
+    int inode_no = -1;
+    inode_no = search_file(pathname, &searched_record);
+    if (inode_no != -1) { // 如果找到了同名目录或文件
+        printk("sys_mkdir: file or directory %s exist!\n", pathname);
+        rollback_step = 1;
+        goto rollback;
+    } else {
+        uint32_t pathname_depth = (uint32_t) path_depth_cnt((char*) pathname);
+        uint32_t path_search_depth = (uint32_t) path_depth_cnt(searched_record.searched_path);
+        // 先判断是否把pathname的各层目录都访问到了,即是否存在某个中间目录查找失败了
+        if (pathname_depth != path_search_depth) {
+            // 说明并没有访问到全部的路径,某个中间目录是不存在的
+            printk("sys_mkdir: can`t access %s, subpath %s is`t exist\n", pathname, searched_record.searched_path);
+            rollback_step = 1;
+            goto rollback;
+        }
+    }
+    struct dir* parent_dir = searched_record.parent_dir;
+    // 目录名称后可能会有字符'/',所以最好直接用searched_record.searched_path,无'/'
+    char* dirname = strrchr(searched_record.searched_path, '/') + 1;
+    inode_no = inode_bitmap_alloc(cur_part);
+    if (inode_no == -1) {
+        printk("sys_mkdir: allocate inode failed\n");
+        rollback_step = 1;
+        goto rollback;
+    }
+
+    struct inode new_dir_inode;
+    inode_init(inode_no, &new_dir_inode); // 初始化inode
+    uint32_t block_bitmap_idx = 0;
+    int32_t block_lba = -1;
+    block_lba = block_bitmap_alloc(cur_part);
+    if (block_lba == -1) {
+        printk("sys_mkdir: block_bitmap_alloc for create directory failed\n");
+        rollback_step = 2;
+        goto rollback;
+    }
+    new_dir_inode.i_sectors[0] = block_lba;
+    // 每分配一个块就将位图同步到硬盘
+    block_bitmap_idx = block_lba - cur_part->sb->data_start_lba;
+    ASSERT(block_bitmap_idx != 0);
+    bitmap_sync(cur_part, block_bitmap_idx, BLOCK_BITMAP);
+
+    // 将当前目录的目录项'.'和'..'写入目录
+    memset(io_buf, 0, SECTOR_SIZE * 2); //清空io_buf
+    struct dir_entry* p_de = (struct dir_entry*)io_buf;
+
+    // 初始化当前目录.
+    memcpy(p_de->filename, ".", 1);
+    p_de->i_no = inode_no;
+    p_de->f_type = FT_DIRECTORY;
+    p_de++;
+    // 初始化当前目录..
+    memcpy(p_de->filename, "..", 2);
+    p_de->i_no = parent_dir->inode->i_no;
+    p_de->f_type = FT_DIRECTORY;
+    // 将目录项同步到数据块
+    ide_write(cur_part->my_disk, new_dir_inode.i_sectors[0], io_buf, 1);
+    new_dir_inode.i_size = 2 * cur_part->sb->dir_entry_size;
+
+    // 在父目录添加自己的目录项
+    struct dir_entry new_dir_entry;
+    memset(&new_dir_entry, 0, sizeof(struct dir_entry));
+    create_dir_entry(dirname, inode_no, FT_DIRECTORY, &new_dir_entry);
+    memset(io_buf, 0, SECTOR_SIZE*2); // 清空io_buf
+    // 将当前目录作为目录项 同步目录项到父目录中
+    if (!sync_dir_entry(parent_dir, &new_dir_entry, io_buf)) {
+        printk("sys_mkdir: sync_dir_entry to disk failed!\n");
+        rollback_step = 2;
+        goto rollback;
+    }
+
+    // 父目录的inode同步到硬盘
+    memset(io_buf, 0, SECTOR_SIZE * 2);
+    inode_sync(cur_part, parent_dir->inode, io_buf);
+    // 将新创建目录的inode同步到硬盘
+    memset(io_buf, 0, SECTOR_SIZE * 2);
+    inode_sync(cur_part, &new_dir_inode, io_buf);
+    // 将inode位图同步到硬盘
+    bitmap_sync(cur_part, inode_no, INODE_BITMAP);
+
+    sys_free(io_buf);
+
+    // 关闭所创建目录的父目录
+    dir_close(searched_record.parent_dir);
+    return 0;
+rollback:	     // 因为某步骤操作失败而回滚
+    switch (rollback_step) {
+        case 2:
+            // 如果新文件的inode创建失败,之前位图中分配的inode_no也要恢复
+            bitmap_set(&cur_part->inode_bitmap, inode_no, 0);
+        case 1:
+            // 关闭所创建目录的父目录
+            dir_close(searched_record.parent_dir);
+            break;
+        default: break;
+    }
+    sys_free(io_buf);
+    return -1;
+}
+
 /** 在磁盘上搜索文件系统,若没有则格式化分区创建文件系统 */
 void filesys_init() {
     uint8_t channel_no = 0, dev_no, part_idx = 0;
--- kernel/main.c
+++ kernel/main.c
@@ -29,7 +29,19 @@ int main(void) {
     process_execute(u_prog_b, "u_prog_b");
 //    thread_start("k_thread_a", 31, k_thread_a, "I am thread_a");
 //    thread_start("k_thread_b", 31, k_thread_b, "I am thread_b");
-    printf("/file1 delete %s!\n", sys_unlink("/file1") == 0 ? "done" : "fail");
+    printf("/dir1/subdir1 create %s!\n", sys_mkdir("/dir1/subdir1") == 0 ? "done" : "fail");
+    printf("/dir1 create %s!\n", sys_mkdir("/dir1") == 0 ? "done" : "fail");
+    printf("now, /dir1/subdir1 create %s!\n", sys_mkdir("/dir1/subdir1") == 0 ? "done" : "fail");
+    int fd = sys_open("/dir1/subdir1/file2", O_CREATE | O_RDWR);
+    if (fd != -1) {
+        printf("/dir1/subdir1/file2 create done!\n");
+        sys_write(fd, "Catch me if you can!\n", 21);
+        sys_lseek(fd, 0, SEEK_SET);
+        char buf[32] = {0};
+        sys_read(fd, buf, 21);
+        printf("/dir1/subdir1/file2 says:\n%s", buf);
+        sys_close(fd);
+    }
     while(1);
     return 0;
 }
